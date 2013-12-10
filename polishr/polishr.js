3840c49cd5d1cf4a4fd4796c190d32d92c22f0a0
Philip James <pjj@philipjohnjames.com>
2013-12-09 21:49:35 -0800

    Made show/hide actually work.

--- polishr/polishr.js
+++ polishr/polishr.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         reblogSmoother();
     });
-    $('.polishr-link').on('click', showPost);
+    $('.polishr-show').on('click', showPost);
 });
 
 var posts = {}
@@ -11,15 +11,28 @@ var posts = {}
 function reblogSmoother(){
     var index = 0;
     $('.post_full, .post').has('blockquote blockquote').each(function(){
-        posts['' + index] = $(this).clone();
-        $(this).html('<p>[Smoothed by Polishr] <a href="javascript:void(0)" class="polishr-link" id="polishr_' + index +'">Show post</a><p>');
         $(this).addClass('polishr_post_' + index);
+        if (!$(this).hasClass('polishr-initialized')) {
+            $(this).addClass('polishr-initialized');
+            $(this).prepend('<p>[Smoothed by Polishr] <a href="javascript:void(0)" class="polishr-hide">Hide Post</a></p>');
+        }
+        posts['' + index] = $(this).clone();
+        if (!$(this).hasClass('polishr-shown')){
+            $(this).html('<p>[Smoothed by Polishr] <a href="javascript:void(0)" class="polishr-show" id="polishr_' + index +'">Show post</a></p>');
+        }
         index = index + 1;
     });
-    $('.polishr-link').on('click', showPost);
+    $('.polishr-show').on('click', showPost);
 }
 
 function showPost(e){
     var id = e.target.id.split('_')[1];
-    $('.polishr_post_' + id).html(posts[id].clone());
+    var selectionString = '.polishr_post_' + id;
+    $(selectionString).replaceWith(posts[id].clone());
+    $(selectionString).addClass('polishr-shown');
+    $('.polishr-hide').on('click', function() {
+        $(this.parentNode.parentNode).removeClass('polishr-shown');
+        $(this.parentNode.parentNode).html('<p>[Smoothed by Polishr] <a href="javascript:void(0)" class="polishr-show" id="polishr_' + id +'">Show post</a></p>');
+        $('.polishr-show').on('click', showPost);
+    });
 }
