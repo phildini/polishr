d477b803d5f46761a00fed6372f6aa35d9f9bd41
Philip James <pjj@philipjohnjames.com>
2013-12-09 20:28:30 -0800

    Add a show-link link to smoothed posts.

--- polishr/polishr.js
+++ polishr/polishr.js
@@ -3,9 +3,23 @@ $(document).ready(function(){
     $(window).scroll(function(){
         reblogSmoother();
     });
+    $('.polishr-link').on('click', showPost);
 });
 
+var posts = {}
+
 function reblogSmoother(){
-    var posts = $('.post_full, .post').has('blockquote blockquote')
-    posts.html('[Smoothed by Polishr]');
+    var index = 0;
+    $('.post_full, .post').has('blockquote blockquote').each(function(){
+        posts['' + index] = $(this).clone();
+        $(this).html('<p>[Smoothed by Polishr] <a href="javascript:void(0)" class="polishr-link" id="polishr_' + index +'">Show post</a><p>');
+        $(this).addClass('polishr_post_' + index);
+        index = index + 1;
+    });
+    $('.polishr-link').on('click', showPost);
+}
+
+function showPost(e){
+    var id = e.target.id.split('_')[1];
+    $('.polishr_post_' + id).html(posts[id].clone());
 }
